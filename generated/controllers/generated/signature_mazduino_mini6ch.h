b45932ae23a0c97bcfde19bc583f8bb2a709ae8d
Amri Karisma <amrikarisma@live.com>
2025-09-23 15:53:15 +0000

    add custom firmware for mazduino_mini6ch

---
+++ generated/controllers/generated/signature_mazduino_mini6ch.h
@@ -0,0 +1,6 @@
+//
+// CHeaderConsumer was generated automatically by rusEFI tool config_definition-all.jar based on gen_config.sh by SignatureConsumer
+//
+
+#define SIGNATURE_HASH 2328465302
+#define TS_SIGNATURE "rusEFI mini6ch.2025.09.23.mazduino_mini6ch.2328465302"
--- knock_config.h
+++ knock_config.h
@@ -4,12 +4,12 @@
 
 #pragma once
 
-// Knock is on ADC1 (PA6 is available on ADC1)
+// Knock is on ADC2 (PC0 is available on ADC2)
 #define KNOCK_ADC ADCD2
 
-// knock 1 - pin PA6
-#define KNOCK_ADC_CH1 ADC_CHANNEL_IN6
-#define KNOCK_PIN_CH1 Gpio::A6
+// knock 1 - pin PC0
+#define KNOCK_ADC_CH1 ADC_CHANNEL_IN10
+#define KNOCK_PIN_CH1 Gpio::C0
 
 // Sample rate & time - depends on the exact MCU
 #define KNOCK_SAMPLE_TIME ADC_SAMPLE_84
